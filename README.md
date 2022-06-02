39a01cf521bdb74185feb9c6d97817cffab64167
howie <howie@git.com>
2022-06-02 15:36:44 +0800

    feat: 1.将VirtualDOM中的文本节点调整为标准结构 2.将不在页面中显示的null/true/false在virtualDOM中剔除出去 3.将children添加到props中

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 
 1. 在.babelrc配置文件中，编写预制代码，告诉babel在解析 jsx 代码时要是用哪个方法。
 2. 创建VirtualDOM，编写craeteElment文件，在src>index.js 中调用 tinyReact 方法并传入 jsx 代码
+3. 在 createElement 中将VirtualDOM的文本节点的结构调整为标准格式
+4. 在 createElement 中判断时候又boolean或者null值，因为页面上并不会显示，所以直接去掉
--- src/TinyReact/createElement.js
+++ src/TinyReact/createElement.js
@@ -6,10 +6,24 @@
  * @return {object} Virtual DOM
  */
 export default function createElement(type, props, ...children) {
+    // {2 === 1 && <div>如果2和1相等渲染当前内容</div>} 使用 reduce 方法把值为null/true/false的去除掉，因为这些并不会在页面中显示。
+    const childElements = [].concat(...children).reduce((result, child) => {
+        if(typeof child !== true || child !== false || child !== null) {
+            // 将对象节点和文本节点分开处理
+            if(child instanceof Object) {
+                result.push(child);
+            } else {
+                result.push(createElement("text", {textContent: child}));
+            }
+        }
+        return result;
+    }, [])
+
     // 返回 VirtualDOM 对象
     return {
         type,
-        props,
-        children
+        // 在React组件中可以使用props.children拿到子结点的，所以这里也是把子节点添加到props中
+        props: Object.assign({children: childElements}, props),
+        children: childElements
     }
 }
